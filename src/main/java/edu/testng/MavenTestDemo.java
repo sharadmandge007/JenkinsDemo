beb9d6a3d871c7968bfea90b6f5726e149d74fb5
Lenovo <Lenovo@LAPTOP-HA1JVESH>
2021-06-15 09:43:25 +0530

    my 1st commit

---
+++ src/main/java/edu/testng/MavenTestDemo.java
@@ -0,0 +1,11 @@
+package edu.testng;
+
+public class MavenTestDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("testng");
+
+	}
+
+}
---
+++ src/test/java/edu/testng/MavenDemoTest.java
@@ -0,0 +1,18 @@
+package edu.testng;
+
+import org.testng.annotations.Test;
+
+public class MavenDemoTest {
+	@Test
+		public void test1()
+		{
+		System.out.println("test1");
+			
+		}
+	@Test
+	public void test2() {
+		System.out.println("test2");
+	}
+	
+
+}
